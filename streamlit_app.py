5f04c0830690c3017afa7f6efd46ba2d1262dfbb
maipatana <maipatana@gmail.com>
2022-11-08 14:02:05 +0000

    initial

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,13 @@ import streamlit as st
 import pandas as pd 
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 st.title('AR686')
 st.write("Web App นี้เป็นส่วนหนึ่งของวิชา AR686 คณะสถาปัตยกรรมศาสตร์มหาวิทยาลัยธรรมศาสตร์")
 df = pd.read_csv('StudentsPerformance.csv')
 
-st.write(df)
+st.dataframe(df)
 
 option = st.selectbox("เลือก Col ที่ต้องการแสดง Group", df.columns)
 
@@ -34,3 +35,9 @@ sns.scatterplot(x="math score", y="reading score", data=df)
 st.pyplot(fig)
 
 #st.write()
+
+df2 = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [13.8, 100.55],
+    columns=['lat', 'lon'])
+
+st.map(df2)
